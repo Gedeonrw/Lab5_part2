b3d9c7e9698d934a016f028fe5010f26acabf021
Gedeon <niyo.gedeon@yahoo.com>
2022-03-13 23:13:13 -0500

    Lab5_Part2

---
+++ app/src/main/java/com/example/lab5_part2/Product.kt
@@ -0,0 +1,17 @@
+package com.example.lab5_part2
+
+internal class Product(
+    title: String,
+    price: Double,
+    color: String,
+    image: String,
+    itemid: String,
+    desc: String
+) {
+    var title = title
+    var price = price
+    var color = color
+    var image = image
+    var itemid = itemid
+    var desc = desc
+}
